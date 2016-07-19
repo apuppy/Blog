8095266f0a713ac1002a99a8920d41fb2ff02f9d
L <chinalihongde@163.com>
2016-07-19 16:55:53 +0800

    去无效download链接

--- download.php
+++ download.php
@@ -4,7 +4,6 @@
     <div class="section">
     <a target="_blank" href="https://www.jetbrains.com/phpstorm">PhpStorm</a>
     <a target="_blank" href="https://www.jetbrains.com/pycharm">PyCharm</a>
-    <a target="_blank" href="Downloads/LICEcap.exe" title="记录屏幕动作为gif文件">gif录像(LICEcap)</a>
     </div>
 </div>
 <?php require_once('footer.php'); ?>
